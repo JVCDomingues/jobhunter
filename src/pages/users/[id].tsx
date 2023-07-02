74527b83e4c69405730559ff8cc88842ab090ff3
João Victor C. Domingues <joao.domingues@b2wdigital.com>
2023-07-02 18:51:51 -0300

    feat(user-page-title): changing user page title to be user's name

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -3,9 +3,9 @@ import { useRouter } from 'next/router';
 import Header from '@/components/Header';
 import LoadingScreen from '@/components/LoadingScreen';
 import { User } from '@/hooks/useUser';
-import { differenceInWeeks } from 'date-fns';
 import { getDateDifference } from './helpers/getDateDifference';
 import { getDateIntervalMessage } from './helpers/getIntervalMessage';
+import Head from 'next/head';
 
 export default function UserPage() {
   const { query } = useRouter();
@@ -37,49 +37,55 @@ export default function UserPage() {
   }
 
   return (
-    <div className="h-screen bg-zinc-50">
-      <Header />
-      {/* Main div */}
-      <div className="p-7">
-        <h1 className="mb-5 text-3xl font-bold">User</h1>
-        <div className="bg-white rounded-md shadow-sm w-full p-5 border border-zinc-300 mb-5">
-          <h1 className="text-xl">{user.name}</h1>
-          <span className="text-sm text-zinc-500">@{user.username}</span>
-        </div>
+    <>
+      <Head>
+        <title>{user?.name}</title>
+      </Head>
+      <div className="h-screen bg-zinc-50">
+        <Header />
+        {/* Main div */}
+        <div className="p-7">
+          <h1 className="mb-5 text-3xl font-bold">User</h1>
+          <div className="bg-white rounded-md shadow-sm w-full p-5 border border-zinc-300 mb-5">
+            <h1 className="text-xl">{user.name}</h1>
+            <span className="text-sm text-zinc-500">@{user.username}</span>
+          </div>
 
-        <h1 className="text-3xl font-bold mb-3">{user?.jobs.length} jobs</h1>
+          <h1 className="text-3xl font-bold mb-3">{user?.jobs.length} jobs</h1>
 
-        <div className="grid lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 gap-3">
-          {user?.jobs?.length > 0 &&
-            user.jobs.map(job => {
-              const differenceInDays = getDateDifference(
-                job.createdAt,
-                new Date()
-              );
+          <div className="grid lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 gap-3">
+            {user?.jobs?.length > 0 &&
+              user.jobs.map(job => {
+                const differenceInDays = getDateDifference(
+                  job.createdAt,
+                  new Date()
+                );
 
-              const intervalMessage = getDateIntervalMessage(differenceInDays);
+                const intervalMessage =
+                  getDateIntervalMessage(differenceInDays);
 
-              return (
-                <div
-                  className="bg-white rounded-md shadow-sm w-full p-5 border border-zinc-300"
-                  key={job.id}
-                >
-                  <div className="flex items-center gap-3">
-                    <h1>{job.name}</h1>
-                    <span className="bg-zinc-700 rounded-full w-1 h-1"></span>
-                    <span className="text-xs bg-cyan-50 p-1 rounded-lg border border-cyan-300 text-cyan-900">
-                      {job.company}
+                return (
+                  <div
+                    className="bg-white rounded-md shadow-sm w-full p-5 border border-zinc-300"
+                    key={job.id}
+                  >
+                    <div className="flex items-center gap-3">
+                      <h1>{job.name}</h1>
+                      <span className="bg-zinc-700 rounded-full w-1 h-1"></span>
+                      <span className="text-xs bg-cyan-50 p-1 rounded-lg border border-cyan-300 text-cyan-900">
+                        {job.company}
+                      </span>
+                    </div>
+                    <span className="text-xs text-zinc-500">
+                      {intervalMessage}
                     </span>
                   </div>
-                  <span className="text-xs text-zinc-500">
-                    {intervalMessage}
-                  </span>
-                </div>
-              );
-            })}
+                );
+              })}
+          </div>
         </div>
+        {/* Main div */}
       </div>
-      {/* Main div */}
-    </div>
+    </>
   );
 }
