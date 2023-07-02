ac2997df4e0acfbffddf8786718fb11283bd42ef
João Victor C. Domingues <joao.domingues@b2wdigital.com>
2023-07-02 17:43:29 -0300

    feat(user-card): adding details button to handle navigation

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -5,9 +5,14 @@ import React from 'react';
 interface UserCardProps {
   user: User;
   handleDeleteButton: () => Promise<void>;
+  handleNavigation: () => void;
 }
 
-export default function UserCard({ user, handleDeleteButton }: UserCardProps) {
+export default function UserCard({
+  user,
+  handleDeleteButton,
+  handleNavigation,
+}: UserCardProps) {
   return (
     <div className="rounded-md border shadow border-zinc-200 w-98 cursor-pointer bg-white">
       <div className="p-5">
@@ -19,14 +24,22 @@ export default function UserCard({ user, handleDeleteButton }: UserCardProps) {
         <span className="text-sm text-zinc-500">
           {user.jobs.length} jobs applied
         </span>
-        <button
-          className="bg-red-500 font-bold px-3 py-2 rounded-md text-white flex items-center gap-3 text-sm hover:bg-red-600 transition-all"
-          onClick={handleDeleteButton}
-          data-testid="delete-button"
-        >
-          <Trash2Icon size={18} />
-          <span>Delete user</span>
-        </button>
+        <div className="flex items-center gap-2">
+          <button
+            className="bg-zinc-600 font-bold px-3 py-2 rounded-md text-white flex items-center gap-3 text-sm hover:bg-zinc-800"
+            onClick={handleNavigation}
+          >
+            View Details
+          </button>
+          <button
+            className="bg-red-500 font-bold px-3 py-2 rounded-md text-white flex items-center gap-3 text-sm hover:bg-red-700 transition-all"
+            onClick={handleDeleteButton}
+            data-testid="delete-button"
+          >
+            <Trash2Icon size={18} />
+            <span>Delete user</span>
+          </button>
+        </div>
       </div>
     </div>
   );
