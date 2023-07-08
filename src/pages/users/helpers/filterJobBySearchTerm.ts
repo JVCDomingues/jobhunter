0b20b44bad3dd0e5b842b9551f03c56e8ae4407c
João Victor C. Domingues <joao.domingues@b2wdigital.com>
2023-07-08 12:11:40 -0300

    feat(search): fixing search helper

--- src/pages/users/helpers/filterJobBySearchTerm.ts
+++ src/pages/users/helpers/filterJobBySearchTerm.ts
@@ -1,8 +1,20 @@
 import { Job } from '@/hooks/useUser';
 
+const filterJob = (term: string, fieldToFilter: string) => {
+  return fieldToFilter.includes(term);
+};
+
 export const filterJobBySearchTerm = (jobs: Job[], searchTerm: string) => {
+  const capitalizedSearchTerm = searchTerm.toLowerCase();
+
   const filteredJobs = searchTerm
-    ? jobs.filter(job => job.name.toLowerCase().includes(searchTerm))
+    ? jobs.filter(
+        job =>
+          filterJob(capitalizedSearchTerm, job.name.toLowerCase()) ||
+          filterJob(capitalizedSearchTerm, job.company.toLowerCase()) ||
+          filterJob(capitalizedSearchTerm, job.modality.toLowerCase()) ||
+          filterJob(capitalizedSearchTerm, job.status.toLowerCase())
+      )
     : jobs;
 
   return filteredJobs;
